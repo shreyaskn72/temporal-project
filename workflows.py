e445c10a6d0a56e9c03670bc3894066228d7cd85
shreyaskn72 <shreyaskn72@gmail.com>
2026-06-04 12:44:11 +0530

    two workflows

--- workflows.py
+++ workflows.py
@@ -5,6 +5,10 @@ from temporalio import activity, workflow
 async def say_hello(name: str) -> str:
     return f"Hello {name} from Temporal!"
 
+@activity.defn
+async def say_morning(name: str) -> str:
+    return f"Good morning {name} sir!"
+
 
 @workflow.defn
 class HelloWorkflow:
@@ -13,6 +17,20 @@ class HelloWorkflow:
     async def run(self, name: str) -> str:
         result = await workflow.execute_activity(
             say_hello,
+            name,
+            start_to_close_timeout=timedelta(seconds=10),
+        )
+
+        return result
+
+
+@workflow.defn
+class GoodMorning:
+
+    @workflow.run
+    async def run_morning(self, name: str) -> str:
+        result = await workflow.execute_activity(
+            say_morning,
             name,
             start_to_close_timeout=timedelta(seconds=10),
         )
